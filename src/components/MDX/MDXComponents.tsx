937dcd6a59650e0513677b6fb10ad9b06e8115d0
iadaria <jadarya@mail.ru>
2024-12-08 16:00:04 +0400

    frameborder

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -447,7 +447,7 @@ function YouTubeIframe(props: any) {
     <div className="relative h-0 overflow-hidden pt-[56.25%]">
       <iframe
         className="absolute inset-0 w-full h-full"
-        frameBorder="0"
+        //frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
         title="YouTube video player"
