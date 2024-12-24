3259b91ab07a311f0c07d9ae01c778f8c84d860b
iadaria <jadarya@mail.ru>
2024-12-24 19:47:51 +0400

    add assetPath to config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const nextConfig = {
     unoptimized: true, // Disable default image optimization
   },
   //basePath: isProd ? '/ru.react.doc' : '',
+  assetPrefix: '/ru.react.doc/',
   basePath: '/ru.react.doc',
   experimental: {
     // TODO: Remove after https://github.com/vercel/next.js/issues/49355 is fixed
