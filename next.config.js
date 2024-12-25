bd46ff7aa6fff2c9f810f31aa73b3a63136e6e4b
iadaria <jadarya@mail.ru>
2024-12-25 13:37:36 +0400

    add env

--- next.config.js
+++ next.config.js
@@ -14,8 +14,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  //basePath: isProd ? '/ru.react.doc' : '',
-  basePath: '/ru.react.doc',
+  basePath: isProd ? '/ru.react.doc' : '',
   //assetPrefix: '/ru.react.doc'
   experimental: {
     // TODO: Remove after https://github.com/vercel/next.js/issues/49355 is fixed
