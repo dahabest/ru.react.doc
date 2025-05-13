b86b6f86e7e65d2bf7a694dd0c81b02d32425424
iadaria <jadarya@mail.ru>
2025-05-13 04:16:33 +0400

    config prod

--- next.config.js
+++ next.config.js
@@ -9,8 +9,10 @@
 
 // http://localhost:3000/ru.react.doc/learn/describing-the-ui/
 
-const isProd = false; // temp
-//const isProd = process.env.NODE_ENV === 'production'; // temp
+// https://dahabest.github.io/ru.react.doc
+
+//const isProd = false; // temp
+const isProd = process.env.NODE_ENV === 'production'; // temp
 
 const nextConfig = {
   output: 'export',
